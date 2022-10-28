dddaf63523131ddc00847ddff9ad3b98a4d721d9
John Cris <johncrismanabo9@gmail.com>
2022-10-28 09:44:50 +0800

    template installed

---
+++ application/views/template/footer.php
@@ -0,0 +1,3 @@
+			<footer class="footer t-a-c"> 
+				© <?php echo date('Y'); ?> Cubic Admin
+            </footer>
---
+++ application/views/template/js.php
@@ -0,0 +1,19 @@
+
+    <!-- ==============================
+        Required JS Files
+    =============================== -->
+    <!-- ===== jQuery ===== -->
+    <script src="<?php echo base_url(); ?>assets/vendor/jquery/dist/jquery.min.js"></script>
+    <!-- ===== Bootstrap JavaScript ===== -->
+    <script src="<?php echo base_url(); ?>assets/vendor/bootstrap/dist/js/bootstrap.min.js"></script>
+    <!-- ===== Slimscroll JavaScript ===== -->
+    <script src="<?php echo base_url(); ?>assets/js/jquery.slimscroll.js"></script>
+    <!-- ===== Wave Effects JavaScript ===== -->
+    <script src="<?php echo base_url(); ?>assets/js/waves.js"></script>
+    <!-- ===== Menu Plugin JavaScript ===== -->
+    <script src="<?php echo base_url(); ?>assets/js/sidebarmenu.js"></script>
+    <!-- ===== Custom JavaScript ===== -->
+    <script src="<?php echo base_url(); ?>assets/js/custom.js"></script>
+    <!-- ===== Plugin JS ===== -->
+    <!-- ===== Style Switcher JS ===== -->
+    <script src="<?php echo base_url(); ?>assets/vendor/styleswitcher/jQuery.style.switcher.js"></script>
