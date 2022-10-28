f87a5c81a583626a14b7b31b6ad3dbbfaa8119d6
John Cris <johncrismanabo9@gmail.com>
2022-10-28 11:15:58 +0800

    view dashboard

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -15,7 +15,7 @@ class Dashboard extends CI_Controller {
     public function index()
 	{ 
         $data['page_title'] = "Dashboard"; 
-        $this->load->view('dashboard', $data);  
+        $this->load->view('admin/dashboard', $data);  
 	}
  
 
