784124d09f8be2ab502964d8b30af6738fedd946
John Cris <johncrismanabo9@gmail.com>
2022-10-28 09:18:05 +0800

    installing template

---
+++ application/views/template/css.php
@@ -0,0 +1,16 @@
+
+    <!-- ===== Bootstrap CSS ===== -->
+    <link href="<?php echo base_url(); ?>assets/vendor/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- ===== Plugin CSS ===== -->
+    <!-- ===== Animation CSS ===== -->
+    <link href="<?php echo base_url() ?>assets/css/animate.css" rel="stylesheet">
+    <!-- ===== Custom CSS ===== -->
+    <link href="<?php echo base_url() ?>assets/css/style.css" rel="stylesheet">
+    <!-- ===== Color CSS ===== --> 
+    <link href="<?php echo base_url() ?>assets/css/colors/green-dark.css" id="theme" rel="stylesheet">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
---
+++ application/views/template/meta.php
@@ -0,0 +1,9 @@
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta name="author" content="">
+	<link rel="shortcut icon" href="<?php echo base_url(); ?>assets/img/favicon.ico" />	
+    <title><?php echo $page_title; ?></title>
