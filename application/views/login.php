ca1ec049cad80b63f3a065023812f4f35efb63f0
John Cris <johncrismanabo9@gmail.com>
2022-10-28 10:10:38 +0800

    update route

--- application/views/login.php
+++ application/views/login.php
@@ -35,7 +35,7 @@
 		<div class="row">
 			<div class="col-md-6 offset-md-3"> 
 				<div class="card my-5"> 
-					<form method="post" action="<?php echo base_url('login/login'); ?>" class="card-body cardbody-color p-lg-5">
+					<form method="post" action="user_login" class="card-body cardbody-color p-lg-5">
 
 						<div class="text-center">
 							<img src="<?php echo base_url(); ?>assets/img/cgdnm-logo.png" class="img-fluid profile-image-pic img-thumbnail rounded-circle my-3"
--- application/views/template/sidebar.php
+++ application/views/template/sidebar.php
@@ -17,7 +17,11 @@
                                 <li role="separator" class="divider"></li>
                                 <li><a href="javascript:void(0);"><i class="fa fa-cog"></i> Account Settings</a></li>
                                 <li role="separator" class="divider"></li> -->
+<<<<<<< HEAD
                                 <li><a href=""><i class="fa fa-power-off"></i> Logout</a></li>
+=======
+                                <li><a href="signout"><i class="fa fa-power-off"></i> Logout</a></li>
+>>>>>>> jc
                             </ul>
                         </div>
                         <p class="profile-text m-t-15 font-16"><a href="javascript:void(0);"> Hanna Gover</a></p>
